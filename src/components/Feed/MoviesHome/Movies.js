3d08995764eb6e28642217fe506fefe4030c9546
Kévin Joya <kevin.joya@outlook.com>
2019-11-13 14:36:53 +0100

    Changed UI

--- src/components/Feed/MoviesHome/Movies.js
+++ src/components/Feed/MoviesHome/Movies.js
@@ -3,39 +3,80 @@ import MoviesStyled from "./MoviesStyled.style";
 
 function Movies() {
 
-  return(
-    <MoviesStyled className="movies--feed">
-      <div class="card">
-        <div className="card-body">
-          <div className="card--img">
-            <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+  return (
+    <MoviesStyled className="movies--feed col-lg-12">
+      <div className="movies--feed--recommendations">
+          <h1>Recommended for you</h1>
+        <div className="row">
+          <div className="card">
+            <div className="card-body">
+              <div className="card--img">
+                <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+              </div>
+              <div className="card-product-infos">
+                <h2>Product name</h2>
+              </div>
+            </div>
           </div>
-          <div class="card-product-infos">
-            <h2>Product name</h2>
+          <div className="card">
+            <div className="card-body">
+              <div className="card--img">
+                <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+              </div>
+              <div className="card-product-infos">
+                <h2>Product name</h2>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <div className="card--img">
+                <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+              </div>
+              <div className="card-product-infos">
+                <h2>Product name</h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="card">
-        <div className="card-body">
-          <div className="card--img">
-            <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+      <div className="movies--feed--top--rated">
+        <h1>Top Rated</h1>
+        <div className="row">
+          <div className="card">
+            <div className="card-body">
+              <div className="card--img">
+                <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+              </div>
+              <div className="card-product-infos">
+                <h2>Product name</h2>
+              </div>
+            </div>
           </div>
-          <div class="card-product-infos">
-            <h2>Product name</h2>
+          <div className="card">
+            <div className="card-body">
+              <div className="card--img">
+                <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+              </div>
+              <div className="card-product-infos">
+                <h2>Product name</h2>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <div className="card--img">
+                <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+              </div>
+              <div className="card-product-infos">
+                <h2>Product name</h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="card">
-        <div className="card-body">
-          <div className="card--img">
-            <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
-          </div>
-          <div class="card-product-infos">
-            <h2>Product name</h2>
-          </div>
-        </div>
-      </div>
+
     </MoviesStyled>
   )
 }
