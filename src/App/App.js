aace2d5efbd717c7752c8905a91e0ceafb511118
Kévin Joya <kevin.joya@outlook.com>
2019-11-13 14:36:32 +0100

    Added some components

--- src/App/App.js
+++ src/App/App.js
@@ -3,20 +3,34 @@ import React from 'react';
 import '../App.css'
 import Title from '../components/Header/Title';
 import Movies from '../components/Feed/MoviesHome/Movies';
+import Trending from '../components/LeftSection/Filters/Trending';
+import TvMovies from '../components/LeftSection/Filters/TvMovies';
+import Watchlist from '../components/LeftSection/Filters/Watchlist';
+import Categories from '../components/LeftSection/Filters/Categories/Categories';
 
 function App() {
   return (
     <div className="App">
       <div className="container col-lg-12">
-        <header className="App-header">
+        {/* <header className="App-header">
           <Title />
-        </header>
-        <div className="right-section col-lg-4">
-
-        </div>
-        <div className="left-section col-lg-8">
-          <Movies />
-        </div>
+        </header> */}
+        <main>
+          <div className="row">
+            <section className="left-section col-lg-2">
+              <Title />
+              <div className="filters">
+                <Trending />
+                <TvMovies />
+                <Watchlist />
+                <Categories />
+              </div>
+            </section>
+            <section className="right-section col-lg-8">
+                <Movies />
+            </section>
+          </div>
+        </main>
       </div>
     </div>
   );
