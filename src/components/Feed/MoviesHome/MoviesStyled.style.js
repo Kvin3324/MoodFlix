cf13acd74e448918aa41edb2c27f6fd1ed23603c
Kévin Joya <kevin.joya@outlook.com>
2019-11-14 11:10:47 +0100

    changed some style

--- src/components/Feed/MoviesHome/MoviesStyled.style.js
+++ src/components/Feed/MoviesHome/MoviesStyled.style.js
@@ -1,28 +1,20 @@
 import styled from "styled-components";
 
 const MoviesStyled = styled.div`
-  {
-    display: grid;
-    // grid-template-columns: 1fr 1fr 1fr;
-    grip-gap: 16px;
 
-    .card {
-      height: 20vh;
-      background: white;
-      box-shadow: 0 0 15px rgba(0,0,0,0.2);
-      padding: 16px;
-
+  .movies--feed--recommendations {
+    display: inline-flex;
       img {
         height: 100%;
-        width: 120px;
+        width: 7vw;
         object-fit: cover;
       }
       h2 {
         font-size: 16px;
         font-weight: bold;
         margin: 0;
+        color: white;
       }
-    }
   }
 `
 
