faca507f5d1e301a94d59daab8247f9243c0795b
Kévin Joya <kevin.joya@outlook.com>
2019-11-14 17:45:52 +0100

    Added carousel on movies

--- src/components/Feed/MoviesHome/MoviesRecommendations.js
+++ src/components/Feed/MoviesHome/MoviesRecommendations.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import MoviesStyled from "./MoviesStyled.style";
+import Slider from "../../Slider/Slider";
+import Movie from "../../Movie/Movie";
 
 function Movies() {
-
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -15,41 +16,27 @@ function Movies() {
 
   return (
     <React.Fragment>
-      <MoviesStyled className="movies--feed col-lg-12">
+      <div className="movies--feed col-lg-12">
         <h1>Recommended for you</h1>
         <div className="movies--feed--recommendations">
+        <Slider>
           {
             function () {
               if (data.length === 0) return null;
 
               if (data.length !== 0) {
                 return data.map((movie, index) => {
-                  return (
-
-                    <div className="mr-5">
-
-                      <div className="movie--img">
-                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="poster" />
-                      </div>
-                      <div className="movie--title">
-                        <h2>{movie.title}</h2>
-                      </div>
-                    </div>
-
-
-
-
+                  return(
+                    <Movie movie={movie} key={index} />
                   )
                 })
               }
             }()
           }
+        </Slider>
         </div>
-
-      </MoviesStyled>
+      </div>
     </React.Fragment>
-
-
   )
 }
 
