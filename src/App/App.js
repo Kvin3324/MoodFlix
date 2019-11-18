938f1f66f694fd41d005980eb3e12c0e0097e9eb
Kévin Joya <kevin.joya@outlook.com>
2019-11-18 17:39:37 +0100

    Added routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Route, HashRouter } from "react-router-dom";
 // import logo from '../logo.svg';
 import '../App.css'
 import Title from '../components/Header/Title';
-// import Movies from '../components/Feed/MoviesHome/MoviesRecommendations';
 import Trending from '../components/LeftSection/Filters/Trending';
 import TvMovies from '../components/LeftSection/Filters/TvMovies';
 import Watchlist from '../components/LeftSection/Filters/Watchlist';
 import Categories from '../components/LeftSection/Filters/Categories/Categories';
 import Feed from '../container/Feed';
+import AboutMovie from "../components/Movie/AboutMovie";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container col-lg-12">
+    <HashRouter>
+      <div className="container App col-lg-12">
         {/* <header className="App-header">
           <Title />
         </header> */}
@@ -28,13 +29,13 @@ function App() {
               </div>
             </section>
             <section className="right-section col-lg-9">
-                {/* <Movies /> */}
-                <Feed />
+              <Route exact path="/" component={Feed} />
+              <Route exact path="/about/:id" component={AboutMovie} ></Route>
             </section>
           </div>
         </main>
       </div>
-    </div>
+    </HashRouter>
   );
 }
 
