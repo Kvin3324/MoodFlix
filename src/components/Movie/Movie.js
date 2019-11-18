a09c4a78727f10f22568c0034a2a6d2bb1f95d6a
Kévin Joya <kevin.joya@outlook.com>
2019-11-18 17:40:47 +0100

    Added link to about route

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,18 +1,24 @@
 import React from "react";
 import MoviesStyled from "../Feed/MoviesHome/MoviesStyled.style";
+import { Link } from "react-router-dom";
+
 
 function Movie(props) {
   return (
-    <MoviesStyled className="movie mr-5">
-    <div className="movie--body mr-5">
-      <div className="movie--img">
-        <img src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} alt="poster" />
-      </div>
-      <div className="movie--title">
-        <h2>{props.movie.title}</h2>
-      </div>
-      </div>
-    </MoviesStyled>
+    <React.Fragment>
+      <MoviesStyled className="movie mr-5" onClick={props.toggleClass}>
+        <Link to={`/about/${props.movie.id}`}>
+          <div className="movie--body mr-5">
+            <div className="movie--img">
+              <img src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} alt="poster" />
+            </div>
+            <div className="movie--title">
+              <h2>{props.movie.title}</h2>
+            </div>
+          </div>
+        </Link>
+      </MoviesStyled>
+    </React.Fragment>
   )
 }
 
