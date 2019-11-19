10ecd804b33b3be321bd3b5b6121797becfcfc1e
Kévin Joya <kevin.joya@outlook.com>
2019-11-19 15:38:15 +0100

    Added style on link title

--- src/components/Header/TitleStyled.style.js
+++ src/components/Header/TitleStyled.style.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 const TitleStyled = styled.h1`
+  margin-bottom: 20%;
+  a, a:hover {
+    color: white;
+    text-decoration: none;
+  }
   font-size: 3em;
   font-family: Lobster;
-  // font-family: cursive;
 `
 
 export default TitleStyled
