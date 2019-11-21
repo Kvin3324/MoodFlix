8bf8f3942f0daa665a3b4e4662ecc719cdab0cce
Kévin Joya <kevin.joya@outlook.com>
2019-11-21 12:22:28 +0100

    Added filters style compo

--- src/components/LeftSection/Filters/Trending/Trending.js
+++ src/components/LeftSection/Filters/Trending/Trending.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FiltersStyled from "../FiltersStyle/FiltersStyled.style";
 
 function Trending() {
   return(
-    <Link to={`/trending`}>
-      <h3>Trending</h3>
-    </Link>
+    <FiltersStyled>
+      <Link to={`/trending`}>
+        <h3>Trending</h3>
+      </Link>
+    </FiltersStyled>
   )
 }
 
