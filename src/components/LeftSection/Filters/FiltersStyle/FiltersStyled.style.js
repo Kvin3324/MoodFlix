351ba43f9bb7fb3020bf15f6f5195ebd3e40bb0c
Kévin Joya <kevin.joya@outlook.com>
2019-11-21 12:23:22 +0100

    Created filters style compo

---
+++ src/components/LeftSection/Filters/FiltersStyle/FiltersStyled.style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const FiltersStyled = styled.div`
+  padding-left: 20px;
+  :hover {
+    border-left: 1px solid #0CD0FC;
+  }
+  a {
+    h3 {
+    font-weight: bold;
+      :hover {
+        color: #0CD0FC;
+      }
+    }
+    :hover {
+      text-decoration: none;
+    }
+  }
+`
+
+export default FiltersStyled
