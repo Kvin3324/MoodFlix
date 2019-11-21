e20e8ed64bc1aa2d321c1f69023b919e39783012
Kévin Joya <kevin.joya@outlook.com>
2019-11-21 18:42:42 +0100

    Fetch api for categories

--- src/components/LeftSection/Filters/Categories/Categories.js
+++ src/components/LeftSection/Filters/Categories/Categories.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import CategoriesStyled from "./CategoriesStyled.style";
 
 function Categories() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY_MOVIES}`)
+    .then(  response => response.json())
+    .then(dataParsed => setData([...dataParsed.genres]))
+  }, [])
+
+  console.log(data);
+
+
+
   return(
     <CategoriesStyled className="categories">
       <h3>Categories</h3>
         <div className="categories--names">
-            <li>Action</li>
-            <li>Comedy</li>
-            <li>Drama</li>
-            <li>Thriller</li>
-            <li>Adventure</li>
-            <li>Science fiction</li>
-            <li>Kebab</li>
-            <li>Mcdo</li>
-            <li>BK</li>
+          {
+            function () {
+              if (data.length === 0) return "loading";
+
+              if (data.length !== 0) {
+                return data.map((genre, index) => {
+                  return <li key={index}>{genre.name}</li>
+                })
+              }
+            }()
+          }
         </div>
     </CategoriesStyled>
   )
