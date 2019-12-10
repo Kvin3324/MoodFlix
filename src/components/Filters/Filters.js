b02fbfdccc74dbf97c547355c8cf8374eb3c0fd7
Kévin Joya <kevin.joya@outlook.com>
2019-12-10 11:43:31 +0100

    Delete compo Watchlist

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Trending from "../LeftSection/Filters/Trending/Trending";
 import TvMovies from "../LeftSection/Filters/TvMovies/TvMovies";
-import Watchlist from "../LeftSection/Filters/Watchlist/Watchlist";
 import Categories from "../LeftSection/Filters/Categories/Categories";
 
 function Filters() {
@@ -9,7 +8,6 @@ function Filters() {
     <React.Fragment>
       <Trending />
       <TvMovies />
-      <Watchlist />
       <Categories />
     </React.Fragment>
   )
