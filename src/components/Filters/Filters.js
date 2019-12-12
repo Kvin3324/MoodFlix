860dbdf3f99642ee0acb44c1953d6d02668573b4
Kévin Joya <kevin.joya@outlook.com>
2019-12-12 14:52:28 +0100

    Added Tv

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -2,12 +2,14 @@ import React from "react";
 import Trending from "../LeftSection/Filters/Trending/Trending";
 import TvMovies from "../LeftSection/Filters/TvMovies/TvMovies";
 import Categories from "../LeftSection/Filters/Categories/Categories";
+import Tv from "../LeftSection/Filters/Tv/Tv";
 
 function Filters() {
   return(
     <React.Fragment>
       <Trending />
       <TvMovies />
+      <Tv />
       <Categories />
     </React.Fragment>
   )
