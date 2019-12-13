cb356113b7deef9ca93b314bead17f098a541869
Kévin Joya <kevin.joya@outlook.com>
2019-12-13 12:06:06 +0100

    Created compo styled season

---
+++ src/components/Tv/SeasonsEp/Seasons/SeasonsStyled.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const SeasonsStyled = styled.div`
+  color: white;
+  border-top: #21232A solid 2px;
+  margin-top: 5%;
+    .season {
+      display: inline-flex;
+          img {
+            width: 15vw;
+          }
+      }
+`
+
+export default SeasonsStyled
