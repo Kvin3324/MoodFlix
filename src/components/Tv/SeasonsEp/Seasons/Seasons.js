cea6936b0616a2012cffba09995aedfdb359b142
Kévin Joya <kevin.joya@outlook.com>
2019-12-13 12:05:53 +0100

    Changed jsx and added fn date

--- src/components/Tv/SeasonsEp/Seasons/Seasons.js
+++ src/components/Tv/SeasonsEp/Seasons/Seasons.js
@@ -1,25 +1,37 @@
 import React from "react";
+import SeasonsStyled from "./SeasonsStyled.style";
 
 function Seasons(props) {
-  console.log("coucou");
-  
-  return(
-    <React.Fragment>
-      <div className="season">
-        <div className="season--poster">
-          <img src={`https://image.tmdb.org/t/p/w500/${props.data.poster_path}`} alt="poster"></img>
-        </div>
-        <div className="season--title">
-          <h2>{props.data.name}</h2>
-        </div>
-        <div className="season--air--date">
-          <span>{props.data.air_date}</span>
-        </div>
-        <div className="season--overview">
-          <p>{props.data.overview}</p>
-        </div>
 
-      </div>
+  function convertDate() {
+    const date = new Date(props.data.air_date);
+    const day = date.getDay();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+
+  return (
+    <React.Fragment>
+      <SeasonsStyled>
+        <div className="season mt-3 mb-3">
+          <div className="season--poster">
+            <img src={`https://image.tmdb.org/t/p/w500/${props.data.poster_path}`} alt="poster"></img>
+          </div>
+          <div className=" ml-3">
+            <div className="season--title">
+              <h2>{props.data.name}</h2>
+            </div>
+            <div className="season--air--date">
+              <p>{convertDate()}</p>
+            </div>
+            <div className="season--overview">
+              <p>{props.data.overview}</p>
+            </div>
+          </div>
+        </div>
+      </SeasonsStyled>
     </React.Fragment>
   )
 }
