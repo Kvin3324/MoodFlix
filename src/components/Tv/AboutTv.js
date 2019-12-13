c3cc5ece44b090a4a47578ca4b6c9aea0f661922
Kévin Joya <kevin.joya@outlook.com>
2019-12-13 12:05:19 +0100

    Added condition for seasons

--- src/components/Tv/AboutTv.js
+++ src/components/Tv/AboutTv.js
@@ -132,64 +132,73 @@ function AboutTv(props) {
     // })
   }
 
-  return (
+
+  return(
     <React.Fragment>
       {
         function () {
           if (data.data.length === 0) return <Loader/>;
 
           if (data.data.length !== 0) {
-            return (
-              <React.Fragment>
-                <SeasonEpStyled>
-                  {/* <span> */}
+            if (data.showSeasons === true) {
+              return(
+                <React.Fragment>
+                  <SeasonEpStyled>
                   <span onClick={tvSeasons}>
-                    {
-                      function () {
-                        if (data.showSeasons === true) {
-                          return data.seasons.map((season, index) => {
-                            return <Seasons data={season} key={index} />
-                          })
-                        }
-                      }()
-                    }
                     Seasons&Episodes
                   </span>
-                  {/* <Link to={"/season&ep"}>
-                    Seasons & Episodes
-                  </Link> */}
-                </SeasonEpStyled>
-                <AboutTvStyled className="tv--about">
-                  <div className="go--back">
-                    <i className="fas fa-home" onClick={goBack}></i>
-                  </div>
-                  <div className="tv--about--header">
-                    <div className="tv--about--backdrop">
-                      <img src={`https://image.tmdb.org/t/p/w500/${data.data.backdrop_path}`} alt="backdrop"></img>
-                    </div>
-                    <div className="tv--about--meta">
-                      <div className="tv--about--title">
-                        <h1>{data.data.original_name}</h1>
-                      </div>
-                      <div className="tv--about--infos">
-                        <div className="tv--about--infos--vote">
-                          {voteAverage()}
-                        </div>
+                     {
+                       function () {
+                         if (data.showSeasons === true) {
+                           return data.seasons.map((season, index) => {
+                             return <Seasons data={season} key={index} />
+                           })
+                         }
+                       }()
+                     }
+                  </SeasonEpStyled>
+                </React.Fragment>
+              )
+            } else {
+              return(
+                <React.Fragment>
 
-                        <div className="tv--about--infos--genres">
-                          <p>Genres:</p>
-                          {tvGenres()}
-                        </div>
-                        <div className="tv--about--infos--runtime">
-                          <p>Episode run time: {data.data.episode_run_time[0]}min</p>
-                        </div>
-                        <div className="tv--about--infos--date">
-                          {releaseYear()}
-                        </div>
-                        <button className="btn btn-primary">
-                          <a href={`https://www.youtube.com/results?search_query=${data.data.original_name}`} rel="noopener noreferrer" target="_blank" >
-                            Watch BA
-                          </a>
+                <SeasonEpStyled>
+                  <span onClick={tvSeasons}>
+                     Seasons&Episodes
+                   </span>
+                  </SeasonEpStyled>
+                <AboutTvStyled className="tv--about">
+                   <div className="go--back">
+                     <i className="fas fa-home" onClick={goBack}></i>
+                   </div>
+                   <div className="tv--about--header">
+                     <div className="tv--about--backdrop">
+                       <img src={`https://image.tmdb.org/t/p/w500/${data.data.backdrop_path}`} alt="backdrop"></img>
+                     </div>
+                     <div className="tv--about--meta">
+                       <div className="tv--about--title">
+                         <h1>{data.data.original_name}</h1>
+                       </div>
+                       <div className="tv--about--infos">
+                         <div className="tv--about--infos--vote">
+                           {voteAverage()}
+                         </div>
+
+                         <div className="tv--about--infos--genres">
+                           <p>Genres:</p>
+                           {tvGenres()}
+                         </div>
+                         <div className="tv--about--infos--runtime">
+                           <p>Episode run time: {data.data.episode_run_time[0]}min</p>
+                         </div>
+                         <div className="tv--about--infos--date">
+                           {releaseYear()}
+                         </div>
+                         <button className="btn btn-primary">
+                           <a href={`https://www.youtube.com/results?search_query=${data.data.original_name}`} rel="noopener noreferrer" target="_blank" >
+                             Watch BA
+                           </a>
                         </button>
                       </div>
                     </div>
@@ -235,13 +244,126 @@ function AboutTv(props) {
                     </div>
                   </div>
                 </AboutTvStyled>
-              </React.Fragment>
-            )
+                </React.Fragment>
+              )
+            }
           }
         }()
       }
     </React.Fragment>
   )
+
+
+  // return (
+  //   <React.Fragment>
+  //     {
+  //       function () {
+  //         if (data.data.length === 0) return <Loader/>;
+
+  //         if (data.data.length !== 0) {
+  //           return (
+  //             <React.Fragment>
+  //               <SeasonEpStyled>
+  //                 {/* <span> */}
+  //                 <span onClick={tvSeasons}>
+  //                   {
+  //                     function () {
+  //                       if (data.showSeasons === true) {
+  //                         return data.seasons.map((season, index) => {
+  //                           return <Seasons data={season} key={index} />
+  //                         })
+  //                       }
+  //                     }()
+  //                   }
+  //                   Seasons&Episodes
+  //                 </span>
+  //                 {/* <Link to={"/season&ep"}>
+  //                   Seasons & Episodes
+  //                 </Link> */}
+  //               </SeasonEpStyled>
+  //               <AboutTvStyled className="tv--about">
+  //                 <div className="go--back">
+  //                   <i className="fas fa-home" onClick={goBack}></i>
+  //                 </div>
+  //                 <div className="tv--about--header">
+  //                   <div className="tv--about--backdrop">
+  //                     <img src={`https://image.tmdb.org/t/p/w500/${data.data.backdrop_path}`} alt="backdrop"></img>
+  //                   </div>
+  //                   <div className="tv--about--meta">
+  //                     <div className="tv--about--title">
+  //                       <h1>{data.data.original_name}</h1>
+  //                     </div>
+  //                     <div className="tv--about--infos">
+  //                       <div className="tv--about--infos--vote">
+  //                         {voteAverage()}
+  //                       </div>
+
+  //                       <div className="tv--about--infos--genres">
+  //                         <p>Genres:</p>
+  //                         {tvGenres()}
+  //                       </div>
+  //                       <div className="tv--about--infos--runtime">
+  //                         <p>Episode run time: {data.data.episode_run_time[0]}min</p>
+  //                       </div>
+  //                       <div className="tv--about--infos--date">
+  //                         {releaseYear()}
+  //                       </div>
+  //                       <button className="btn btn-primary">
+  //                         <a href={`https://www.youtube.com/results?search_query=${data.data.original_name}`} rel="noopener noreferrer" target="_blank" >
+  //                           Watch BA
+  //                         </a>
+  //                       </button>
+  //                     </div>
+  //                   </div>
+  //                 </div>
+  //                 <div className="tv--about--description">
+  //                   <h2>SYNOPSIS</h2>
+  //                   <p>{data.data.overview}</p>
+  //                 </div>
+  //                 <div className="tv--season mb-5">
+  //                   <div className="tv--season--number">
+  //                     <p> {data.data.number_of_seasons} </p>
+  //                     <p>seasons</p>
+  //                   </div>
+  //                   <span className="separator"></span>
+  //                   <div className="tv--season--number">
+  //                     <p> {data.data.number_of_episodes}</p>
+  //                     <p>episodes</p>
+  //                   </div>
+  //                 </div>
+  //                 <div className="episodes col-lg-12 mt-5">
+  //                   <div className="row">
+  //                     <div className="about--episodes col-lg-6">
+  //                       <div className="about--episodes--last">
+  //                         <h3>Last episode:</h3>
+  //                         <div className="about--episodes--last--img mb-4">
+  //                           {lastEpImg()}
+  //                         </div>
+  //                         <div className="about--episodes--last--title">
+  //                           <h4> {data.data.last_episode_to_air.name} </h4>
+  //                         </div>
+  //                         <div className="about--episodes--last--number">
+  //                           <p>episode number: {data.data.last_episode_to_air.episode_number} </p>
+  //                         </div>
+  //                         <div className="about--episodes--last--episode">
+  //                           <p>release number: {data.data.last_episode_to_air.air_date} </p>
+  //                         </div>
+  //                       </div>
+  //                     </div>
+
+  //                     <div className="about--episodes col-lg-6">
+  //                       {nextEp()}
+  //                     </div>
+  //                   </div>
+  //                 </div>
+  //               </AboutTvStyled>
+  //             </React.Fragment>
+  //           )
+  //         }
+  //       }()
+  //     }
+  //   </React.Fragment>
+  // )
 }
 
 export default AboutTv
