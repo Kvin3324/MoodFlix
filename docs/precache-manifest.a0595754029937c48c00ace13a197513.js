42344ad06df8fa47b760afedcecc5c412706b33c
Kévin Joya <kevin.joya@outlook.com>
2019-12-16 15:19:43 +0100

    Added folder to deploy prod

---
+++ docs/precache-manifest.a0595754029937c48c00ace13a197513.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9597797e69efe6192d8efa1f294d0f36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db81305676c8eca38b2",
+    "url": "/static/css/2.46abd18e.chunk.css"
+  },
+  {
+    "revision": "0a37ae26798693b5077b",
+    "url": "/static/css/main.53b8662a.chunk.css"
+  },
+  {
+    "revision": "4db81305676c8eca38b2",
+    "url": "/static/js/2.1041f4d8.chunk.js"
+  },
+  {
+    "revision": "0a37ae26798693b5077b",
+    "url": "/static/js/main.35a2815d.chunk.js"
+  },
+  {
+    "revision": "b01a2f923cc301512acf",
+    "url": "/static/js/runtime-main.f54d7c24.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
