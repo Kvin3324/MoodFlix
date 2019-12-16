236d6b636258894d2bc49a3bcd7b6d98b6f28597
Kévin Joya <kevin.joya@outlook.com>
2019-12-16 15:28:00 +0100

    Cleaned code

--- src/container/Feed.js
+++ src/container/Feed.js
@@ -3,16 +3,6 @@ import MoviesRecommendations from "../components/Feed/MoviesHome/MoviesRecommend
 import MoviesTopRated from "../components/Feed/MoviesHome/MoviesTopRated";
 
 function Feed() {
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(`https://api.themoviedb.org/3/movie/12/recommendations?api_key=${process.env.REACT_APP_API_KEY_MOVIES}&language=en-US&page=1`)
-  //   .then(response => response.json())
-  //   .then(dataParsed => setData([...dataParsed.results]))
-  // }, []);
-
-  // console.log(data);
-
   return(
     <React.Fragment>
       <MoviesRecommendations />
